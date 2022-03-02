fa56fa78e8aa77ca065b93b887baea79025cf888
Marcos Aurélio <100322505+marcoslopes22@users.noreply.github.com>
2022-03-02 18:32:09 -0300

    Create Descrição.py

---
+++ DesafiosPythonIniciante/Descrição.py
@@ -0,0 +1,7 @@
+#BY: MARCOS AURELIO
+
+def bemVindo(boasVindas):
+  boasVindas = "Estarei armazendo alguns desafios simples ultilizando a linguagem python")
+  print(boasVindas)
+  
+bemVindo(boasVindas);
